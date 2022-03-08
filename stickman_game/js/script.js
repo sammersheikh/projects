b0aa472c54cbbd9ffa42accb7505b3e8492bc23a
sammersheikh <sammer.sheikh@outlook.com>
2022-03-07 22:03:35 -0800

    Added win/loss logic. Game works

--- stickman_game/js/script.js
+++ stickman_game/js/script.js
@@ -1,36 +1,28 @@
 /*----- constants -----*/
-const img1 = `<img src='https://i.postimg.cc/6QxQYhnS/stickman1.png' border='0' alt='stickman1'/>` 
-const img2 = `<img src='https://i.postimg.cc/C1FxVLvs/stickman2.png' border='0' alt='stickman2'/>`
-const img3 = `<img src="https://i.postimg.cc/xCtTjDhD/stickman3.png" alt="stickman3"/>`
-const img4 = `<img src="https://i.postimg.cc/C52hDCwc/stickman4.png" alt="stickman4"/>`
-const img5 = `<img src="https://i.postimg.cc/wjLqysZk/stickman5.png" alt="stickman5"/>`
-const img6 = `<img src="https://i.postimg.cc/Bv3qxnGf/stickman6.png" alt="stickman6"/>`
-const img7 = `<img src="https://i.postimg.cc/k4XqpY4j/stickman7.png" alt="stickman7"/>`
-const img8 = `<img src="https://i.postimg.cc/qBF41LdL/stickman8.png" alt="stickman8"/>`
-const img9 = `<img src="https://i.postimg.cc/mgyrpHjg/stickman9.png" alt="stickman9"/>`
-const img10 = `<img src="https://i.postimg.cc/nhWT2m6s/stickman.png" alt="stickman"/>`
 
 const imgArr = [
     ``,
-    `<img src='https://i.postimg.cc/6QxQYhnS/stickman1.png' border='0' alt='stickman1'/>`,
-    `<img src='https://i.postimg.cc/C1FxVLvs/stickman2.png' border='0' alt='stickman2'/>`,
-    `<img src="https://i.postimg.cc/xCtTjDhD/stickman3.png" alt="stickman3"/>`,
-    `<img src="https://i.postimg.cc/C52hDCwc/stickman4.png" alt="stickman4"/>`,
-    `<img src="https://i.postimg.cc/wjLqysZk/stickman5.png" alt="stickman5"/>`,
-    `<img src="https://i.postimg.cc/Bv3qxnGf/stickman6.png" alt="stickman6"/>`,
-    `<img src="https://i.postimg.cc/k4XqpY4j/stickman7.png" alt="stickman7"/>`,
-    `<img src="https://i.postimg.cc/qBF41LdL/stickman8.png" alt="stickman8"/>`,
-    `<img src="https://i.postimg.cc/mgyrpHjg/stickman9.png" alt="stickman9"/>`,
-    `<img src="https://i.postimg.cc/nhWT2m6s/stickman.png" alt="stickman"/>`,
+    `<img src='https://i.postimg.cc/6QxQYhnS/stickman1.png' id="stickImg" border='0' alt='stickman1'/>`,
+    `<img src='https://i.postimg.cc/C1FxVLvs/stickman2.png' id="stickImg" border='0' alt='stickman2'/>`,
+    `<img src="https://i.postimg.cc/xCtTjDhD/stickman3.png" id="stickImg" alt="stickman3"/>`,
+    `<img src="https://i.postimg.cc/C52hDCwc/stickman4.png" id="stickImg" alt="stickman4"/>`,
+    `<img src="https://i.postimg.cc/wjLqysZk/stickman5.png" id="stickImg" alt="stickman5"/>`,
+    `<img src="https://i.postimg.cc/Bv3qxnGf/stickman6.png" id="stickImg" alt="stickman6"/>`,
+    `<img src="https://i.postimg.cc/k4XqpY4j/stickman7.png" id="stickImg" alt="stickman7"/>`,
+    `<img src="https://i.postimg.cc/qBF41LdL/stickman8.png" id="stickImg" alt="stickman8"/>`,
+    `<img src="https://i.postimg.cc/mgyrpHjg/stickman9.png" id="stickImg" alt="stickman9"/>`,
+    `<img src="https://i.postimg.cc/nhWT2m6s/stickman.png" id="stickImg" alt="stickman"/>`,
 ]
-
 
 const words = ['apple', 'matrix', 'subway', 'boggle', 'oxidize', 'ivy', 'zombie', 'pixel'];
 const underscore = '_ ';
+
 /*----- app's state (variables) -----*/
 
-let guessedLetter, currentWord, wordLength, blankWord, wins, indexOfLetter = null;
+let guessedLetter, currentWord, wordLength, blankWord, indexOfLetter, splitWord, splitBlankWord = null;
 let wrongGuesses = 0;
+let wins = 0;
+let losses = 0;
 let guessedWord = []
 let currentWordArr = []
 
@@ -43,15 +35,13 @@ let currentWordArr = []
 $('#generateBtn').on('click', generateWord);
 
 // Submit button listener --> calls getInput function
-$('#submitBtn').on('click', getInput);
-
-
+$('#submitBtn').on('click', submitGuess);
 
 /*----- functions -----*/
 
 
-// Generate random word function
 
+// Generate random word function
 function generateWord() {
     currentWord = words.pop(); 
     wordLength = currentWord.length
@@ -59,45 +49,71 @@ function generateWord() {
     $('h1.underscore').html(blankWord)
     separateCurrentWord()
     separateBlankWord()
+    restartGame()
 }
 
 // Get input from user function (on event listener for submit button)
-
-function getInput() {
+function submitGuess() {
     guessedLetter = $('#input').val();
     $('#input').val('');
     isLetterCorrect();
+    winLoss()
 }
 
 // separates the currentWord and blankWord strings into letters/underscores into separate arrays 
-let splitWord = null;
 function separateCurrentWord() {
     splitWord = currentWord.split('');
 }
 
-let splitBlankWord = null;
 function separateBlankWord() {
     splitBlankWord = blankWord.split(/[' ']/)
     splitBlankWord.pop()
 }
-
 // Check if guessed letter is in splitWord (generated word that is split into letters in an array)
-// Will get called in the getInput() function
+// Will get called in the submitGuess() function
 function isLetterCorrect() {
     indexOfLetter = splitWord.findIndex(function(letter) {
         return letter === guessedLetter;
     })
-
     if (indexOfLetter === -1) {
-        alert('Wrong! Guess again.')
+        // alert('Wrong! Guess again.') // -------> Add better notification system
         wrongGuesses++;
+        if ($("div.stickman:has(img)").length > 0) {
+            $('#stickImg').remove()
+        }
         $('div.stickman').append(imgArr[wrongGuesses]);
+        $('#guessedLetters').append(`${guessedLetter}, `)
     } else if (indexOfLetter !== -1) {
-        alert('CORRECT!')
+        // alert('CORRECT!') // ---------------> Add better notification system
+        splitBlankWord[indexOfLetter] = guessedLetter;
+        const correctGuess = splitBlankWord.join(' ');
+        $('h1.underscore').html(correctGuess);
     }
 
 }
 
-
-// Add image when guess is wrong
-// $('div.stickman').append(img1); <-- How to display the stickman 
+// Restart game function when generate word is clicked (clears the board)
+function restartGame() {
+    $('#guessedLetters').text('');    
+    if ($("div.stickman:has(img)").length > 0) {
+    $('#stickImg').remove()
+    }
+    wrongGuesses = 0;
+}
+let correctGuess = null; //DELETE GLOBAL VARIABLE - - DECLARE IN FUNCTION BELOW
+// Win/Loss function (calls restartGame() if wrongGuesses > 9)
+function winLoss() {
+    correctGuess = splitBlankWord.find(function(char) {
+        return char === '_'
+    })
+    if (wrongGuesses > 9) {
+        console.log('loser')
+        losses++ // ---------------> Not incrementing - returns NaN
+        $('#losses').html(losses) // ^
+        alert('You LOSE!')
+    } else if (!correctGuess) {
+        console.log('winner')
+        wins++
+        $('#wins').html(wins)
+    }
+}
