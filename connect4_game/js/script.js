a6a06f4432634fc9812baae86859dcbea1b85a1b
sammersheikh <sammer.sheikh@outlook.com>
2022-03-09 13:42:12 -0800

    1. Player can click board piece and piece will be selected and filled with that players color. 2. Added logic when player clicks game piece to update gameboard array with matching index of piece selected. 3. Added logic to prevent current player to change game piece of already selected game piece from previous player

--- connect4_game/js/script.js
+++ connect4_game/js/script.js
@@ -2,12 +2,12 @@
 
 const gameBoard = [
     
-    ['brd0','brd1','brd2','brd3','brd4','brd5','brd6',
+    'brd0','brd1','brd2','brd3','brd4','brd5','brd6',
     'brd7','brd8','brd9','brd10','brd11','brd12','brd13',
     'brd14','brd15','brd16','brd17','brd18','brd19','brd20',
     'brd21','brd22','brd23','brd24','brd25','brd26','brd27',
     'brd28','brd29','brd30','brd31','brd32','brd33','brd34',
-    'brd35','brd36','brd37','brd38','brd39','brd40','brd41']
+    'brd35','brd36','brd37','brd38','brd39','brd40','brd41'
 
 ]
 
@@ -16,7 +16,7 @@ const player2 = -1;
 
 /*----- app's state (variables) -----*/
 
-let selectedPiece, spotTaken = null;
+let selectedPiece, spotTaken, idx = null;
 let playerTurn = -1;
 
 /*----- cached element references -----*/
@@ -32,16 +32,21 @@ $('tbody tr').on('click','div', selectColumn);
 // selectColumn() function will make each column clickable
 //  ^(called by event listener for the circle divs)
 function selectColumn(evt) {
-    // selectedPiece = parseInt(evt.target.id)  
-    selectedPiece = evt.target.id
-    playerTurn = (playerTurn * -1) 
-    gameBoard[0][selectedPiece] = playerTurn;
-    $(`#${selectedPiece}`).attr("background-color","blue")
-    // if (playerTurn === 1) {
-    //     //fill background-color of selected div with blue
-        
-    // } else if (playerTurn === -1) {
-    //     //fill background-color of selected div with red
-    // }
-    
+    selectedPiece = evt.target.id.match(/\d+/) //returns first occurence of numbers from a string
+    idx = gameBoard.find(function(idx) {
+        return idx === (gameBoard[selectedPiece])
+    }) 
+    if (idx === 1 || idx === -1) {
+        alert('That spot is taken')   
+    } else if (idx !== 1 || idx !== -1) {
+        playerTurn = (playerTurn * -1)
+        gameBoard[selectedPiece] = playerTurn;
+        if (playerTurn === 1) {
+            console.log('player1')
+            $(`#${evt.target.id}`).css("background-color","blue")
+        } else if (playerTurn === -1) {
+            console.log('player2')
+            $(`#${evt.target.id}`).css("background-color","red")
+        }
+    }
 }
